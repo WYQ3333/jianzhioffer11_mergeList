25235769bbe5c5500d23cc01b37961f2d6adf419
浅忆梦微凉 <3538922590@qq.com>
2019-01-17 08:52:57 +0800

    合并两个有序单链表编译通过

--- 剑指offer11_合并两个排序的链表/test.cpp
+++ 剑指offer11_合并两个排序的链表/test.cpp
@@ -46,10 +46,53 @@ public:
 		}
 	}
 	
-	//ListNode* Merge(ListNode* pHead1, ListNode* pHead2)
-	//{
+	//合并两个有序单链表
+	//方法一
 
-	//}
+	ListNode* Merge(ListNode* pHead1, ListNode* pHead2)
+	{
+		ListNode* NpHead = nullptr;
+		ListNode* Ncur = NpHead;
+		ListNode* cur1 = pHead1;
+		ListNode* cur2 = pHead2;
+		if (pHead1 == nullptr&&pHead2 == nullptr){
+			return nullptr;
+		}
+		if (pHead1 == nullptr&&pHead2!=nullptr){
+			NpHead = pHead2;
+			return NpHead;
+		}
+		if (pHead2 == nullptr&&pHead1!=nullptr){
+			NpHead = pHead1;
+			return NpHead;
+		}
+
+		while (cur1 != nullptr&&cur2 != nullptr){
+			if (cur1->val <= cur2->val){
+				if (NpHead == nullptr){
+					NpHead = cur1;
+				}
+				else{
+					Ncur->next = cur1;
+					Ncur->next->next = nullptr;
+					cur1 = cur1->next;
+					Ncur = Ncur->next;
+				}
+			}
+			if (cur1->val > cur2->val){
+				if (NpHead == nullptr){
+					NpHead = cur2;
+				}
+				else{
+					Ncur->next = cur2;
+					Ncur->next->next = nullptr;
+					cur2 = cur2->next;
+					Ncur = Ncur->next;
+				}
+			}
+		}
+		return NpHead;
+	}
 
 public:
 	ListNode * _pHead;
